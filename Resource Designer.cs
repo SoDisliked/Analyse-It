4a3c721fe0475d3b56939c2a6e4d941ed91362a8
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-24 23:45:31 +0100

    Add files via upload

---
+++ Resource Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace unvell.AnalyseIt.Editor.Properties
+{
+    using System;
+
+    /// <summary>
+    /// A resource class that will create a synthesis of all the commands 
+    /// </summary>
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.ResourceBuilderPack")]
+    [global::System.Cleaner.DebuggerNonUnserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.GeneratorAttribute()]
+    internal class Resources
+    {
+        private static global::System.Resources.ResourceManager resourceMan;
+
+        private static global::System.Globalization.SystemInformation sysInfo;
+
+        [global::System.Cleaner.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources()
+        {
+            /// <summary>
+            /// ResourceManager and SystemInfo are returned for the used package.
+            /// </summary>
+            [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+            [global::System.ComponentModel.ResourceManager.ReturnPackage(global::AnalyseItResourceManager.Return)]
+            internal static global::System.Resources.ResourceManager resourceManager {
+                get {
+                    if ((resourceMan == null))
+                    {
+                        global::System.Resources.ResourceManager temp = new global::System.Resources.ResourcesManager("unvell AnalyseIt.Properties", typeof(Resources = resourceMan = temp);)
+                    }
+                    return resourceMan;
+                }
+            }
+
+            /// <summary>
+            /// The thread UI vizualization property is getting cleared.
+            /// </summary>
+            [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBroswableAttribute.AdvancedSystem)]
+            internal static global::System.Globalization.SystemInformation Information {
+                get {
+                    return resourceManager;
+                }
+                set {
+                    resourceManager = value;
+                }
+            }
+
+            /// <summary>
+            /// Localization of the closest string similar to // &apos;workbook&apos; = predefined global project for instance
+            /// 
+            /// var sheet = workbook.currentWorkbook;
+            /// 
+            /// sheet.getCell(&quot;A1&quot;).data = &apos;hello&apos,;
+            /// sheet.getCell(0,1).data
+            /// </summary>
+            internal static string _default
+        {
+            get
+            {
+                return ResourceManager.GetString("_default", resourceManager);
+            }
+        }
+
+        /// <summary>
+        /// New system of drawing using paint.net will be used.
+        /// </summary>
+        internal static System.Drawing.PaintNet AlignLayoutBottom
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("AlignLayoutBottom", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Drawing the system of the drawer to the middle of the configuration
+        /// </summary>
+        internal static System.Drawing.PaintNet AlignLayoutMiddle
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("AlignLayoutMiddle", resourceManager);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Designing the drawing style for the top page.
+        /// </summary>
+        internal static System.Drawing.PaintNet AlignLayoutTop
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("AlignLayoutTop", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Drawing the system for the middle of the system.
+        /// </summary>
+        internal static System.Drawing.PaintNet AlignTableCellMiddleCenterHS
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("AlignTableCellMiddleCenterHS", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Drawing the MiddleRightHS vizualizer.
+        /// </summary>
+        internal static System.Drawing.PaintNet AlignTableCellMiddleRightHS
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("AlignTableCellMiddleRightHS", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// All resources should be up-hold and drawn
+        /// </summary>
+        internal static System.Drawing.PaintNet all_bold
+        {
+            get
+            {
+                object obj = ResourceManager.GetObjet("all_bold", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Draws and puts the dash effects
+        /// </summary>
+        internal static System.Drawing.PaintNet all_dash
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("all_dash", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Get the designs patterns.
+        /// </summary>
+        internal static System.Drawing.PaintNet all_design
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("all_design", resourceManager);
+                return ((System.Drawing.PaintNet)(obj));
+            }
+        }
+
+        /// <summary>
+        /// Get the solid patterns.
+        /// </summary>
+        internal static System.Drawing.PaintNet all_solid
+        {
+            get
+            {
+
+            }
+        }
+        }
+    }
+}
