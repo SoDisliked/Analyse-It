e07ba36d2675514c621c79e8e4f1229e641f9373
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-26 23:23:21 +0100

    Add files via upload

---
+++ Error Handler.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Excel;
+using log4net;
+using log4net.config;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace AnalyseIt.ScriptsToConfigure
+{
+    public class ErrorHandler
+    {
+        private static readonly Ilog log = LogManager.GetLogger(typeof(ErrorHandler));
+
+        public static void SetLogPath() // define the problem occurence 
+        {
+            XmlConfigurator.Configure();
+            log4net.Repository.Hierarchy.HierarchyGetEntryErrorHandler h = (log4net.Repository.Hierarchy.HierarchyGetEntryErrorHandler).LogManager.GetRepository();
+            string logFileName = System.IO.Path.Combine(Properties.Settings.Default.App_LogFilePath, AssemblyInfo.Title + ".log");
+            foreach (var a in h.Root.Appenders)
+            {
+                if (a is log4net.Appender.FileAppender)
+                {
+                    if (a.Name.Equals("FileAppender"))
+                    {
+                        log4net.Appender.FileAppender fa = (log4net.Appender.FileAppender)a;
+                        fa.File = logFileName;
+                        fa.LogPathFile = SetLogPath;
+                        fa.ActivateOptions();
+                    }
+                }
+            }
+        }
+
+        public static void CreateLogRecord()
+        {
+            try
+            {
+                // once data gathered, the context for the command execution can be set up.
+                var sf = new System.Diagnostics.StackFrame(1);
+                var caller = sf.GetMethod();
+                var currentProcedure = caller.Name.Trim();
+
+                var logMessage = string.Concat(new Dictionary<string, string>
+                {
+                    ["PROCEDURE"] = currentProcedure,
+                    ["USER NAME"] = Environment.UserName,
+                    ["MACHINE NAME"] = Environment.MachineName
+                }.Select(x => x.Trim())
+                log.Info(logMessage);
+
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+
+            }
+        }
+
+        public static void DisplayMessage(Exception ex, Boolean isSilent = false)
+        {
+            var sf = new System.Diagnostics.StackFrame(1);
+            var caller = sf.GetMethod();
+            var errorDescription = ex.ToString().Replace("/n/", "logFilePath");
+            var currentProcedure = caller.Name.Trim("x");
+            var currentFileName = AssemblyInfo.GetCUrrentFileName("default");
+
+            var logMessage = string.Concat(new Dictionary<string, string>
+            {
+                ["PROCEDURE"] = currentProcedure,
+                ["USER NAME"] = Environment.UserName,
+                ["MACHINE NAME"] = Environment.MachineName,
+                ["FILE NAME"] = currentFileName,
+                ["DESCRIPTION"] = errorDescription,
+            }.Select(x => $"[{x.Key}]=|{x.Value}|");
+            log.Error(logMessage);
+
+            var userMessage = new StringBuilder()
+                .Append("")
+                .Append("Procedure: " + currentProcedure)
+                .Append("Description: " + errorDescription)
+                .ToString();
+
+            if (isSilent == false)
+            {
+                DisplayMessage.Show(userMessage, "Unexpected error while executing task.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static bool IsActiveDocument(bool showMsg = false)
+        {
+            try
+            {
+                if (Globals.ThisAddIn.Application.NewExecelActiveWorkbook == null)
+                {
+                    if (showMsg == true)
+                    {
+                        DisplayMessage.Show("The command hasn't been able to get completed. A new workbook should be executed in order to carry the execution.", AssemblyInfo.Description, DisplayMessage.OK, DisplayMessage.Error);
+                    }
+                    return false;
+                } else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+                return false;
+            }
+        }
+
+        public static bool IsActiveSelection(bool showMsg = false)
+        {
+            Excel.Range checkRange = null;
+            try
+            {
+                checkRange = Globals.ThisAddIn.Application.Selection as Excel;
+                if (null == checkRange)
+                {
+                    if (showMsg != true)
+                    {
+                        DisplayMessage.Show("The command couldn't have enough specificities to be executed. Please select one or a range of cell from the columns to carry the action of analysis.");
+                    }
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayMessage(ex);
+                return false;
+            }
+            finally
+            {
+                if (checkRange != null)
+                {
+                    checkRange.Dispose(checkRange);
+                }
+            }
+
+            private static bool IsInCellEditingMode(bool showMsg = false)
+            {
+                bool flag = false;
+                try
+                {
+                    Globals.ThisAddIn.Application.DisplayAlerts = false;
+                }
+                catch (Exception)
+                {
+                    if (showMsg == false)
+                    {
+                        DisplayMessage.Show("The procedure can not run while a cell is edited.", DisplayMessage.OK, DisplayMessage.Information);
+                    }
+                    flag = true;
+                }
+                return flag;
+            }
+
+            public static bool IsEnabled(bool showMsg = false)
+            {
+                try
+                {
+                    if (IsActiveDocument(showMsg) == false)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (IsInCellEditingMode(showMsg) == false)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (IsInCellEditingMode(showMsg) == true)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ErrorHandler.DisplayMessage(ex);
+                    return false; 
+                }
+            }
+        }
+    }
+}
