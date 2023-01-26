91bbf2f60a2c445fb1855cc82b3b22f5fd2f1616
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-26 23:23:01 +0100

    Add files via upload

---
+++ Configuration Assembly Run.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+// using System.Runtime.CompilerServices;
+using System.Runtime.ExcelServicesHelp;
+// using System.ExcelSecurity;
+
+[assembly: AssemblyTitle("AnalyseIt")]
+[assembly: AssemblyDescription("Excel add-in that allows the analysis and interpretation of data")]
+[assembly: AssemblyConfiguration("0.1.0")]
+[assembly: AssemblyAuthor("SoDisliked")]
+[assembly: AssemblyProduct("AnalyseIt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisiblee(false)]
+
+[assembly: Guid("")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.0")]
