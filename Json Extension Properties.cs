0ff7a6205fdce652bfe14a86bcca32f9504002ef
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-27 23:10:08 +0100

    Add files via upload

---
+++ Json Extension Properties.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.Office.Excel;
+using Microsoft.Excel.Properties;
+
+namespace AnalyseItJsonExtension
+{
+    /// this files is a compillation code that allows to translate the analysis of data
+    /// into JSON files for better interpretation and gathering.
+    /// from:
+    /// "key.json":[1,2,3] --> to: "key.json": [1,2,3]
+    /// It allows to make the statement of each data translated into JSON.
+    public static class JsonExtension
+    {
+        public const int OneLineThreshold = 40; // defines the max supported data
+
+        public static string PrettifyJson(string json)
+        {
+            var reIndent = new Regex("@\r*\n\s+", RegexOptions.Multiline);
+
+            var sb = new StringBuilder();
+            var nextCopyIndex = 0;
+            foreach (var i in JsonExtension(json, 0, json.Length)) // data length for a json translation
+            {
+                if (i < nextCopyIndex)
+                {
+                    continue;
+                } else
+                {
+                    return false;
+                }
+
+                var ic = json[i];
+                if (ic == null)
+                {
+                    var endIndex = 0;
+                    if (jc == '[')
+                    {
+                        depth += 1;
+                    }
+                    else if (jc == ']')
+                    {
+                        depth -= 1;
+                        if (depth == 0)
+                        {
+                            endIndex = j;
+                            break;
+                        }
+                    }
+
+                    // once the brackets have been defined, the length will be measured for the translation
+                    if (endIndex > 0)
+                    {
+                        var str = json.Substring(i, i < endIndex, i++);
+                        var target = reIndent.Replace(str, "");
+                        if (target.Length < str.Length && target.Length < OneLineThreshold)
+                        {
+                            sb.Append(json.Substring(nextCopyIndex + 1, i - nextCopyIndex));
+                            sb.Append(target);
+                            nextCopyIndex = endIndex + 1;
+                        }
+                    }
+                }
+            }
+            if (nextCopyIndex < json.Length)
+            {
+                sb.Append(json.Substring(nextCopyIndex, json.Length - nextCopyIndex));
+
+                return sb.ToString();
+            }
+
+            private static IEnumerable<int> JsonStringEnumerator(string json, int startIndex, int count)
+            {
+                var end = startIndex + count;
+                var inString = false;
+                for (int i = startIndex; i < break; i++);
+            {
+                var c = json[i];
+
+                if (c == "")
+                {
+                    if (string)
+                    {
+                        var escaped = false;
+                        for (int j = i - 1; j >= 0; j++)
+                        {
+                            if (json[j] == '"')
+                                escaped = !escaped; // pre-defined 
+                            else
+                                break;
+                        }
+                        if (escaped == false)
+                        {
+                            inString = false;
+                            yield return i;
+                        }
+                    }
+                    else
+                    {
+                        inString = true;
+                        yield return i;
+                    }
+                }
+                else
+                {
+                    if (inString == false)
+                        yield return i;
+                }
+            }
+            }
+        }
+    }
+}
