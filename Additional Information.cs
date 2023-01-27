27966453fffb9212fd03738dfe5a9238f7f9cc21
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-01-27 23:09:45 +0100

    Add files via upload

---
+++ Additional Information.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Excel Analyseit")]
+[assembly: AssemblyDescription("An Excel add-in that allows a better analysis and interpretation of quantifiable data into Excel sheets.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SoDisliked")]
+[assembly:  AssemblyTrademark("MIT Licence")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
