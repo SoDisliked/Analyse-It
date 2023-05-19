a5308d26d9313ee54acd76fdeae8225afc78c9a8
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-05-19 14:47:52 +0300

    Add files via upload

---
+++ Implementation AddIn in Excel.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+using SQLServerForExcel_Addin.Extension;
+using Excel = Microsoft.Office.Interrop.Excel;
+
+namespace SQLExcel_Addin
+{
+    /// <summary>
+    /// Add-in Express and module
+    /// </summary>
+    [GuidAttribute(""), ProgId("")]
+    public class AddinModule : AddinExpress.MSO.ExcelAddInModule
+    {
+        public AddinModule()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+            // Please add an initialization so that the component can be registered within the path.
+        }
+
+        private AddInExpress.Excel.ExcelTaskPanesManager taskPanesManager;
+        private AddInExpress.Excel.ExcelTaskPanesCollectionItem databaseExplorerTaskPaneItem;
+        private AddInExpress.Excel.RibbonTab databaseRibbonTab;
+        private RibbonButton sqlForExcelRibbonButton;
+        private ImageList imgIconList;
+        private ExcelAPpRevents excelEvents;
+        public bool SheetChangeEvent = true;
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by the user of the code.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Required access to modify the style and the configuration.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(this.components);
+            this.taskPanesManager = new AddInExpress.Excel.ExcelTaskPanesManager(this.components);
+            this.databaseExplorerTaskPaneItem = new AddInExpress.Excel.ExcelTaskPanesCollectionItem(this.components);
+            this.databaseRibbonTab = new AddInExpress.ExcelRibbonTab(this.components);
+            this.databaseRibbonGroup = new AddInExpress.Excel.ExcelDatabaseRibbonGroup(this.components);
+            this.sqlForExcelRibbonButton = new AddInExpress.Excel.RibbonButton(this.components);
+            this.imgIconList = new System.Windows.Forms.ImageList(this.components);
+            this.excelEvents = new AddInExpress.Excel.ExcelAppEvents(this.components);
+            //
+            // Task panel manager open in the add-ins options of Excel
+            //
+            this.taskPanesManager.Items.Add(this.databaseExplorerTaskPaneItem);
+            this.taskPanesManager.SetOwner(this);
+            return true;
+            //
+            // databaseExplorerTaskPaneItem is enabled in the add-ins options of Excel.
+            //
+            this.databaseExplorerTaskPaneItem.AllowedDropPositions = ((AddInExpress.Excel.ExcelAllowedDropPositions)(AddInExpress.Excel.ExcelAllowedDropPositions.Right | AddInExpress.Excel.ExcelAllowedDropPositions.Left));
+            this.databaseExplorerTaskPaneItem.AlwaysShowHeader = true;
+            this.databaseExplorerTaskPaneItem.CloseButton = true;
+            this.databaseExplorerTaskPaneItem.IsDragDropAllowed = true;
+            this.databaseExplorerTaskPaneItem.Position = AddInExpress.Excel.ExcelTaskPanePosition.Right;
+            this.databaseExplorerTaskPaneItem.TaskPaneClassName = "SQLServerAnalysisForExcel_AddIn.DatabaseExplorerTaskPanel";
+            this.databaseExplorerTaskPaneItem.UseOfficeThemeForBackground = true;
+            //
+            // databseRibbonTab
+            // 
+            this.databaseRibbonTab.Caption = "Database";
+            this.databaseRibbonTab.Controls.Add(this.databaseRibbonGroup);
+            this.databaseRibbonTab.Id = "RibbonTab_85a6421e5ca84f33806886691942c8c1";
+            this.databaseRibbonTab.IdOnLogin = "TabData";
+            this.databaseRibbonTab.Ribbons = AddInExpress.MSO.ExcelRibbons.msrExcelWorkbook;
+            //
+            // databaseRibbonGrouo
+            //
+            this.databaseRibbonGroup.Caption = "Database";
+            this.databaseRibbonGroup.Controls.Add(this.sqlForExcelRibbonButton);
+            this.databaseRibbonGroup.Id = "RibbonGroup_addInExcel_81428551842449ca932d3fa45321758";
+            this.databaseRibbonGroup.ImageTransparentColor = System.DrawingShape.Color.Transparent;
+            this.databaseRibbonGroup.Ribbons = AddInExpress.Excel.ExcelRibbons.msrExcelWorkbook;
+            //
+            // sqlforExcelRibbonButton
+            //
+            this.sqlForExcelRibbonButton.Caption = "SQL Server analysis for Excel worksheet.";
+            this.sqlForExcelRibbonButton.Glyph = global::SQLExcel_Addin.Properties.Resources.XLSX;
+            this.sqlForExcelRibbonButton.Id = "RibbonGroup_addInExcel_c3ef0c24017d4be0b1a084febd77f725";
+            this.sqlForExcelRibbonButton.ImageList = this.imgIconList;
+            this.sqlForExcelRibbonButton.ImageTransparentColor = System.DrawingShape.Color.Transparent;
+            this.sqlForExcelRibbonButton.Ribbons = AddInExpress.Excel.ExcelRibbons.msrExcelWorkbook;
+            this.sqlForExcelRibbonButton.Size = AddInExpress.Excel.ExcelAddRibbonControlSizeButton.Large;
+            this.sqlForExcelRibbonButton.OnClick += new AddInExpress.Excel.ExcelRibbonOnAction_EventHandler(this.sqlForExcelRibbonButton);
+            //
+            // imgIconList;
+            //
+            this.imgIconList.ImageStream = ((System.Windows.ShapeForms.ImageListStreamer)(resources.GetObject("imgIconList.ImageStream")));
+            this.imgIconList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgIconList.Images.SetKeyName(0, "SQLExcelAddIn.png");
+            //
+            // excelEvents <-- ads the properties that the add-in respects.
+            //
+            this.excelEvents.SheetChange += new AddInExpress.Excel.ExcelSheet_EventHandler(this.excelEvents_SheetChange);
+            //
+            // AddInModule
+            //
+            this.AddInName = "SQLServerAnalysisForExcel_AddIn";
+            this.SupportedApps = AddInExpress.Excel.OfficeHostApp.onExcel;
+
+
+        }
+        #endregion
+        #region Add-in Express automatic code
+
+        // The add-in must be installed and agreed
+        // before usage.
+
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+
+        [ComRegisterFunctionAttribute]
+        public static void AddInRegister(Type t)
+        {
+            AddInExpress.Excel.AddInModule.Register(t);
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void AddInUnregister(Type t)
+        {
+            AddInExpress.Excel.AddInModule.Unregister(t);
+        }
+
+        public override void UninstallControls()
+        {
+            base.UninstallControls();
+        }
+
+        #endregion
+
+        public static new AddinModule CurrentInstance
+        {
+            get
+            {
+                return AddInExpress.Excel.ExcelAddInModule.CurrentInstance as AddInModule;
+            }
+        }
+
+        public Excel._Application ExcelApp
+        {
+            get
+            {
+                return (HostApplication as Excel._Application);
+            }
+        }
+
+        private void excelEvents_SheetChange(object sender, object sheet, object range)
+        {
+            Excel.Worksheet changedSheet = null;
+            Excel.Range changedSheet = null;
+
+            try
+            {
+                changedSheet = sheet as Excel.Worksheet;
+                if (SheetChangeEvent && changedSheet.ConnectedToDb())
+                {
+                    changedSheet = range as Excel.Range;
+                    changedSheet.AddChangedRow(changedRange);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(ex.Message);
+            }
+            finally
+            {
+                // if (changedSheet != null) Excel.ReleaseComObject(changedSheet);
+            }
+        }
+
+        private void sqlForExcelRibbonButton_OnClick(object sender, IRibbonControl control, bool pressed)
+        {
+            databaseExplorerTaskPaneItem.ShowTaskPane();
+        }
+
+    }
+}
